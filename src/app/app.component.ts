55c7849bb624a5ad677025005d2e4f42e709cd4c
Jos Vermoesen <info@rv.be>
2020-11-22 19:30:16 +0100

    Init before upgrade to Angular 10

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from './global/language.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+
+  constructor(private ls: LanguageService) {}
+
+  ngOnInit() {
+    this.ls.setInitialAppLanguage();
+  }
+}
